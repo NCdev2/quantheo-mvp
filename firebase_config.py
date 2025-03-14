ff3a68d614d2b9c498ad865fecfe2b530fc92520
NCdev2 <nevelle.coutinho@gmail.com>
2025-03-14 21:40:45 +0530

    Fix KeyError for missing FIREBASE key in secrets

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,11 @@
+import streamlit as st
+
 firebase_config = {
-    "apiKey": "AIzaSyB0u5Zjndy9io6C5BfbYeUrdV1tJ6zThEM",
-    "authDomain": "user-registeration-f2de5.firebaseapp.com",
-    "databaseURL": "https://user-registeration-f2de5-default-rtdb.firebaseio.com",  # ✅ Add this line
-    "projectId": "user-registeration-f2de5",
-    "storageBucket": "user-registeration-f2de5.appspot.com",
-    "messagingSenderId": "1077504794296",
-    "appId": "1:1077504794296:web:742acce0d5085bcc655e4d",
-    "measurementId": "G-Q123T3R90C"
+    "apiKey": st.secrets["FIREBASE"]["API_KEY"],
+    "authDomain": st.secrets["FIREBASE"]["AUTH_DOMAIN"],
+    "projectId": st.secrets["FIREBASE"]["PROJECT_ID"],
+    "storageBucket": st.secrets["FIREBASE"]["STORAGE_BUCKET"],
+    "messagingSenderId": st.secrets["FIREBASE"]["MESSAGING_SENDER_ID"],
+    "appId": st.secrets["FIREBASE"]["APP_ID"],
+    "measurementId": st.secrets["FIREBASE"]["MEASUREMENT_ID"]
 }
